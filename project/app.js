b42623ef124ec6dcdffe88c040f85ce0939f02ee
luuducvy <luuvy15899@gmail.com>
2026-03-06 22:57:20 +0700

    Cập nhật

--- project/app.js
+++ project/app.js
@@ -10,10 +10,132 @@ getTask = () => {
   return JSON.parse(localStorage.getItem("tasks"));
 };
 
+// add task
+addTask = () => {
+  const btnAddTask = document.querySelector(".add-task");
+  const inputTask = document.querySelector(".input-task");
+  const alertBox = document.querySelector(".alert-box");
+  btnAddTask.addEventListener("click", function () {
+    let tasks = getTask() || [];
+
+    if (this.getAttribute("data-index") === "") {
+      if (inputTask.value.trim() === "") {
+        alertBox.innerHTML = "Không để trống Task";
+        setTimeout(() => {
+          alertBox.innerHTML = "";
+        }, 3000);
+        return;
+      }
+
+      // Add task
+      let newTask = {
+        name: inputTask.value.trim(),
+        status: false,
+      };
+
+      tasks.unshift(newTask);
+      saveTask(tasks);
+      inputTask.value = "";
+      alertBox.innerHTML = `<div class="text-green-700"> Thêm Task thành công ^^</div>`;
+      setTimeout(() => {
+        alertBox.innerHTML = "";
+      }, 3000);
+      renderTask();
+    }else{
+
+      // edit Task
+      tasks[this.getAttribute('data-index')].name = inputTask.value;
+      btnAddTask.innerHTML = 'Add Task'
+      this.setAttribute('data-index','');
+      inputTask.value = '';
+      saveTask(tasks);
+      renderTask();
+      alertBox.innerHTML = `<div class="text-green-700"> Cập nhật thành công ^^</div>`;
+      setTimeout(() => {
+        alertBox.innerHTML = "";
+      }, 3000);
+    }
+  });
+};
+
+// destroy task
+destroyTask = () => {
+  const destroyBtn = document.querySelectorAll(".destroy-task");
+  const alertBox = document.querySelector(".alert-box");
+  let tasks = getTask() || [];
+
+  for (let item of destroyBtn) {
+    item.addEventListener("click", function () {
+      const index = this.getAttribute("data-index");
+      tasks.splice(index, 1);
+      saveTask(tasks);
+      countTask();
+      renderTask();
+      alertBox.innerHTML = `<div class="text-blue-600"> Xóa task thành công !</div>`;
+      setTimeout(() => {
+        alertBox.innerHTML = "";
+      }, 5000);
+    });
+  }
+};
+
+// Task complete
+taskComplete = () => {
+  const taskName = document.querySelectorAll(".name-task");
+  const taskCount = document.querySelector(".count-task-complete");
+  let tasks = getTask() || [];
+  for (let element of taskName) {
+    element.addEventListener("click", function () {
+      const index = this.getAttribute("data-index");
+      if (tasks[index].status === true) {
+        tasks[index].status = false;
+        this.classList.remove("line-through");
+      } else {
+        tasks[index].status = true;
+        this.classList.add("line-through");
+      }
+
+      let count = countTask();
+      renderTask();
+      saveTask(tasks);
+      
+    });
+  }
+};
+
+// editTask
+editTask = () => {
+  const editTask = document.querySelectorAll(".edit-task");
+  const btnAddTask = document.querySelector(".add-task");
+  const inputTask = document.querySelector(".input-task");
+  let tasks = getTask() || [];
+  for (let element of editTask) {
+    element.addEventListener("click", function () {
+      const index = this.getAttribute("data-index");
+      console.log(index);
+      inputTask.value = tasks[index].name;
+      btnAddTask.innerHTML = "Edit Task";
+      btnAddTask.setAttribute("data-index", index);
+    });
+  }
+};
+
+// countTask
+countTask = () =>{
+  const countTask = document.querySelector('.count-task-complete');
+  const tasks = getTask() || [];
+  let count = 0;
+  for(let element of tasks){
+    if(element.status == true) count++;
+  }
+  countTask.innerHTML = `<span class="text-xs text-gray-500 font-semibold"> Đã hoàn thành ( ${count} ) </span>`
+  return count;
+}
+
 // Render task
 renderTask = () => {
   const taskList = document.querySelector(".task-list");
-  const tasks = getTask();
+  let tasks = getTask() || [];
   let taskRender = tasks.map((element, index) => {
     return `
         <div class="py-2 flex justify-between items-center">
@@ -28,101 +150,12 @@ renderTask = () => {
   });
   taskList.innerHTML = taskRender.join("");
 
-  saveTask(tasks);
-  addTask();
   destroyTask();
+  taskComplete();
+  editTask();
   taskComplete();
 };
 
-// add task
-addTask = () => {
-  const btnAddTask = document.querySelector(".add-task");
-  const inputTask = document.querySelector(".input-task");
-  const alertBox = document.querySelector(".alert-box");
-  btnAddTask.addEventListener("click", () => {
-    let tasks = getTask();
-    // Add task
-    if (inputTask.value.trim() === "") {
-      alertBox.innerHTML = "Không để trống task !";
-      return;
-    } else {
-      let newTask = {
-        name: inputTask.value.trim(),
-        status: false,
-      };
-
-      tasks.push(newTask);
-      saveTask(tasks);
-      inputTask.value = "";
-      alertBox.innerHTML = `<div class="text-green-700"> Thêm Task thành công ^^</div>`;
-      renderTask();
-    }
-  });
-};
-
-// destroy task
-destroyTask = () => {
-  const destroyBtn = document.querySelectorAll(".destroy-task");
-  const alertBox = document.querySelector(".alert-box");
-  let tasks = getTask();
-
-  for (let item of destroyBtn) {
-    item.addEventListener("click", function () {
-      const index = this.getAttribute("data-index");
-      tasks.splice(index, 1);
-      saveTask(tasks);
-      renderTask();
-      alertBox.innerHTML = `<div class="text-blue-600"> Xóa task thành công !</div>`;
-      setTimeout(() => {
-        alertBox.innerHTML = "";
-      }, 5000);
-    });
-  }
-};
-
-// Task complete
-taskComplete = () => {
-  const taskName = document.querySelectorAll(".name-task");
-  const taskCount = document.querySelector(".count-task-complete");
-  const tasks = getTask();
-  for (let element of taskName) {
-    element.addEventListener("click", function () {
-      const index = this.getAttribute("data-index");
-      if (tasks[index].status === true) {
-        tasks[index].status = false;
-        this.classList.remove("line-through");
-        count--;
-        // taskCount.innerHTML = `<span class="mt-3"> Đã hoàn thành (${count}) </span>`;
-      } else {
-        tasks[index].status = true;
-        this.classList.add("line-through");
-        count++;
-        // taskCount.innerHTML = `<span class="mt-3"> Đã hoàn thành (${count}) </span>`;
-      }
-      saveTask(tasks);
-    });
-  }
-};
-
-// editTask
-// editTask = () => {
-//   const editTask = document.querySelectorAll(".edit-task");
-//   const btnAddTask = document.querySelector(".add-task");
-//   const inputTask = document.querySelector(".input-task");
-//   const tasks = getTask();
-//   for (let element of editTask) {
-//     element.addEventListener("click", function () {
-//       const index = this.getAttribute("data-index");
-//       inputTask.setAttribute("value", tasks[index].name);
-//       btnAddTask.innerHTML = "Edit Task";
-//       btnAddTask.setAttribute("data-index", index);
-//     });
-//   }
-// };
-
-renderTask();
 addTask();
-destroyTask();
-taskComplete();
+renderTask();
 countTask();
-// editTask();
